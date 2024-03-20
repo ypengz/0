47570c615d66863ece7fcb988fc87ebe616f71e3
yipengzhang <162081103+ypengz@users.noreply.github.com>
2024-03-21 05:29:55 +0800

    Update encoder1.py
    Adding decoder functionality to the first bit of code

--- encoder1.py
+++ encoder1.py
@@ -4,12 +4,19 @@ def encode(password):
         encoded += str((int(char) + 3) % 10)
     return encoded
 
+def decode(encoded_password):
+    decoded = ''
+    for char in encoded_password:
+        decoded += str((int(char) - 3) % 10)
+    return decoded
+
 def main():
     while True:
         print("\nMenu")
         print("-------------")
         print("1. Encode")
-        print("2. Quit")
+        print("2. Decode")
+        print("3. Quit")
         
         option = input("Please enter an option: ")
         
@@ -18,12 +25,17 @@ def main():
             encoded_pw = encode(pw)
             print("Your password has been encoded and stored!")
             print(f"The encoded password is {encoded_pw}.")
-        
+    
         elif option == '2':
+            encoded_pw = input("Please enter your encoded password to decode: ")
+            original_pw = decode(encoded_pw)
+            print(f"The encoded password is {encoded_pw} and the original password is {original_pw}.")
+        
+        elif option == '3':
             print("bye!")
             break
         else:
             print("error.")
 
 if __name__ == "__main__":
-    main()
+    main
